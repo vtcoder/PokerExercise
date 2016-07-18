1d5c58c9c906b33ff7612ea702ce14ee31ebb0cf
John Doe <jdoe@local>
2016-07-18 16:48:28 -0400

    Added support for loading card input from a file. Hooked up the CLI.

--- Poker.Console/Program.cs
+++ Poker.Console/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Poker.Domain.Entities;
+using Poker.Domain.Logic;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,44 @@ namespace Poker.Console
 {
     class Program
     {
+        private const string INVALID_FILE_ARG_MESSAGE = "A valid file path must be specified.";
+
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Poker Score Calculator");
+
+            try
+            {
+                string filePath = GetFilePath(args);
+
+                CardParser parser = new CardParser();
+                IEnumerable<Card> cards = parser.LoadCardsFromFile(filePath);
+
+                ScoreCalculator scoreCalculator = new ScoreCalculator();
+                Score score = scoreCalculator.CalculateScore(cards);
+
+                System.Console.WriteLine("\nThe calculated poker score was:");
+                System.Console.WriteLine(score);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("\nAn error occurred while attempting to calculate the score: {0}", ex.Message);
+            }
+
+            System.Console.WriteLine("\nPress any key to quit.");
+            System.Console.ReadKey();
+        }
+
+        static string GetFilePath(string[] args)
+        {
+            if (args == null || args.Length < 1 || string.IsNullOrWhiteSpace(args[0]))
+                throw new ArgumentException(INVALID_FILE_ARG_MESSAGE);
+
+            string filePath = args[0];
+            if (!File.Exists(filePath))
+                throw new ArgumentException(INVALID_FILE_ARG_MESSAGE);
+
+            return filePath;
         }
     }
 }
--- Poker.Domain.Logic/CardParser.cs
+++ Poker.Domain.Logic/CardParser.cs
@@ -1,6 +1,7 @@
 ﻿using Poker.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,23 @@ namespace Poker.Domain.Logic
                     Value = cardInput[0].FromSingleInput(),
                     Suit = (Suit)Enum.Parse(typeof(Suit), cardInput[1].ToString())
                 };
+            return cards;
+        }
+
+        public string[] LoadCardInputFromFile(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line = reader.ReadLine();
+                string[] cardInputs = line.Split(new char[] { ' ' });
+                return cardInputs;
+            }
+        }
+
+        public IEnumerable<Card> LoadCardsFromFile(string filePath)
+        {
+            string[] cardInputs = LoadCardInputFromFile(filePath);
+            IEnumerable<Card> cards = Parse(cardInputs);
             return cards;
         }
     }
