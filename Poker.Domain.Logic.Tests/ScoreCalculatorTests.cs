c6229f9d5275a5423ebd8948154baac1122d8134
John Doe <jdoe@local>
2016-07-18 14:37:24 -0400

    Initial commit of solution and project structure.

---
+++ Poker.Domain.Logic.Tests/ScoreCalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Poker.Domain.Logic.Tests
+{
+    [TestClass]
+    public class ScoreCalculatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
